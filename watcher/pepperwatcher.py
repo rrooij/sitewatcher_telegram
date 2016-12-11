bdfe6a63da70c95e84e72330095377dd2d7e0a04
rrooij <rderooij685@gmail.com>
2016-12-11 12:37:51 +0100

    Initial commit

---
+++ watcher/pepperwatcher.py
@@ -0,0 +1,32 @@
+import requests
+import os.path
+from bs4 import BeautifulSoup
+
+from watcher.watcher import Watcher
+
+class PepperWatcher(Watcher):
+
+    filename = 'site_pepper.txt'
+
+    def parse_site(self):
+        search_keyword = 'prijsfout'
+        cookies = dict(sort_by="eyJpdiI6InhLQVwvVjdpV01Sblc5clZJV3FUT1FDdDIxdm1OUHRFM2V0SnBtSmhOK2ZBPSIsInZhbHVlIjoiU1RKV3EwSzM0Q0QwSitOUkwrVzVNZUpPeVBMbEhqWEVMcCtlbldiWm1FOD0iLCJtYWMiOiJlZTI3Yzc2MGU3YzEwZTg1NDRjMDVhMWU5ZGE0MDkwYzU3YTAxNWU0OWQyZWRlOTg5NmUxOTgzYzA4M2QxOGQ3In0=")
+        result = requests.get("https://nl.pepper.com/search?q={0}".format(search_keyword), cookies=cookies)
+        content = result.content
+        soup = BeautifulSoup(content, 'html.parser')
+        return soup.find_all('a', class_='thread-title-text', limit=1)[0].get_text()
+
+    def check_price_error(self):
+        articles = self.parse_site()
+        message_text = 'PRIJSFOUT GEVONDEN: {0}'.format(articles)
+
+        if not os.path.isfile(self.filename):
+            self.write_to_file(self.filename, articles)
+            exit(0)
+        else:
+            with open(self.filename, 'r') as f:
+                file_content = f.read().replace('\n', '')
+
+                if file_content != articles:
+                    self.send_telegram(message_text)
+            self.write_to_file(self.filename, articles)
---
+++ watcher/tweakerswatcher.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import os.path
+
+from watcher.watcher import Watcher
+
+class TweakersWatcher(Watcher):
+
+    filename = 'site_tweakers.txt'
+
+    def parse_site(self):
+        url = 'https://tweakers.net/xmlhttp/xmlHttp.php?application=tweakbase&type=filter&action=deals&fromHash=1&currFilters=q1ZKSaz0T0srTi1RsjLUUcpNrAhKzUksySxLDSjKTE51KcovgEhk5jkmFefnlJYgSxgZgGWcS4uKUvNKwBJKVhAxMKcYpheLoQZ6ZmCpsMzUcqA6g1oA&output=json';
+        request = requests.get(url)
+        json_object = json.loads(request.text)
+        return json_object['data']['html']
+
+
+    def check_price_error(self):
+        url = 'https://tweakers.net/pricewatch/deals/#filter:q1ZKSaz0T0srTi1RsjLUUcpNrAhKzUksySxLDSjKTE51KcovgEhk5jkmFefnlJYgSxgZgGWcS4uKUvNKwBJKVhAxMKcYpheLoQZ6ZmCpsMzUcqA6g1oA'
+        message_text = 'Mogelijke prijsfout op Tweakers, check: {0}'.format(url)
+        html = self.parse_site()
+        if not os.path.isfile(self.filename):
+            self.write_to_file(self.filename, html)
+            exit(0)
+        else:
+            with open(self.filename, 'r') as f:
+                file_content = f.read()
+                if file_content != html:
+                    self.send_telegram(message_text)
+            self.write_to_file(self.filename, html)
