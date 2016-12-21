c33140d3f4c13d667818e794062c206abdde898b
rrooij <rderooij685@gmail.com>
2016-12-21 20:14:33 +0100

    pepperwatcher: add search keyword for 'fout'

--- watcher_app.py
+++ watcher_app.py
@@ -3,5 +3,7 @@ from watcher.tweakerswatcher import TweakersWatcher
 
 pepper = PepperWatcher()
 pepper.check_price_error()
+pepper.search_keyword = 'fout'
+pepper.check_price_error()
 tweakers = TweakersWatcher()
 tweakers.check_price_error()
