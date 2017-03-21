029fd4c0b8b4a35472ec4c966e969ba6039a4e62
rrooij <rderooij685@gmail.com>
2017-03-21 11:08:50 +0100

    pepperwatcher: set url in message

--- watcher/pepperwatcher.py
+++ watcher/pepperwatcher.py
@@ -8,6 +8,7 @@ class PepperWatcher(Watcher):
     watcher_name = 'Pepper'
     filename = ''
     search_keyword = 'prijsfout'
+    url = ''
 
     def generate_filename(self):
         self.filename = 'site_pepper_{0}.txt'.format(self.search_keyword)
@@ -15,14 +16,15 @@ class PepperWatcher(Watcher):
     def parse_site(self):
         self.generate_filename()
         cookies = dict(sort_by="eyJpdiI6InhLQVwvVjdpV01Sblc5clZJV3FUT1FDdDIxdm1OUHRFM2V0SnBtSmhOK2ZBPSIsInZhbHVlIjoiU1RKV3EwSzM0Q0QwSitOUkwrVzVNZUpPeVBMbEhqWEVMcCtlbldiWm1FOD0iLCJtYWMiOiJlZTI3Yzc2MGU3YzEwZTg1NDRjMDVhMWU5ZGE0MDkwYzU3YTAxNWU0OWQyZWRlOTg5NmUxOTgzYzA4M2QxOGQ3In0=")
-        result = requests.get("https://nl.pepper.com/search?q={0}".format(self.search_keyword), cookies=cookies)
+        self.url = "https://nl.pepper.com/search?q={0}".format(self.search_keyword)
+        result = requests.get(self.url, cookies=cookies)
         content = result.content
         soup = BeautifulSoup(content, 'html.parser')
         return soup.find_all('a', class_='thread-title-text', limit=1)[0].get_text()
 
     def check_price_error(self):
         articles = self.parse_site()
-        message_text = 'Zoekterm: {0} | *PRIJSFOUT GEVONDEN OP PEPPER*: {1}'.format(self.search_keyword, articles)
+        message_text = 'Zoekterm: {0} | *PRIJSFOUT GEVONDEN OP PEPPER*: {1} URL: {2}'.format(self.search_keyword, articles, self.url)
 
         if not os.path.isfile(self.filename):
             self.write_to_file(self.filename, articles)
