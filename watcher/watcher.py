0e9735cd507623beaf4d9ad31845ac9e6efcea02
rrooij <rderooij685@gmail.com>
2019-02-17 23:06:15 +0100

    Add bol

--- watcher/watcher.py
+++ watcher/watcher.py
@@ -14,8 +14,5 @@ class Watcher:
             payload = { 'chat_id': chat_id, 'parse_mode': 'Markdown', 'text': '[{0}]: {1}'.format(watcher_name, message) }
             requests.get("https://api.telegram.org/bot{0}/sendMessage".format(config.bot_password), params=payload)
 
-    def parse_site(self):
-        raise NotImplementedError("Implement this method")
-
     def check_price_error(self):
         raise NotImplementedError("Implement this method")
--- watcher_app.py
+++ watcher_app.py
@@ -1,3 +1,4 @@
+from watcher.bolwatcher import BolWatcher
 from watcher.pepperwatcher import PepperWatcher
 from watcher.tweakerswatcher import TweakersWatcher
 
@@ -7,3 +8,5 @@ pepper.search_keyword = 'fout'
 pepper.check_price_error()
 tweakers = TweakersWatcher()
 tweakers.check_price_error()
+bol = BolWatcher()
+bol.check_price_error()
